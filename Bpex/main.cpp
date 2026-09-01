55481830b537f12cd64f01499e0c76e1fae84c6b
C0WN0W <imcownow@yeah.net>
2026-09-01 19:53:16 +0800

    驱动测试

---
+++ Bpex/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "Driver/mainClass.h"
+
+int main()
+{
+	if (Driver::InstallDriver()) {
+		std::cout << "驱动安装成功！" << std::endl;
+	} else {
+		std::cerr << "驱动安装失败！" << std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
